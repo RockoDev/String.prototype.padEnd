bbd67c719ce4524f884aa639aee86ba8a332f18b
RockoDev <contacto@rockodev.com>
2017-06-08 22:11:52 -0500

    Add support for older browsers

--- padEnd.polyfill.js
+++ padEnd.polyfill.js
@@ -1,11 +1,10 @@
-
 (function() {
   'use strict';
   if ('padEnd' in String.prototype) return;
   String.prototype.padEnd = function(len, str) {
     str = str === undefined ? '\u0020' : String(str);
     if (len <= this.length || str.length === 0) return String(this);
-    var buffer = new Array(len-this.length);
+    var buffer = new Array(len-this.length); str = str.split('');
     for (var i = 0; i < buffer.length; i++) buffer[i] = str[i%str.length];
     return this.concat(buffer.join(''));
   };
